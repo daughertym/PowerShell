739f2fc3aff89235d8f1d89bb66e8a002eebf88b
daughertym <66864919+daughertym@users.noreply.github.com>
2020-07-17 23:56:32 -0500

    Update Confirm-HotFix.ps1
    Confirm if a certain hot fix is installed on one or more computers.

--- Confirm-HotFix.ps1
+++ Confirm-HotFix.ps1
@@ -65,10 +65,16 @@ param (
 
 begin {
 
-    # Make sure InvokeParallel switch is not being used while piping input
+    # Make sure InvokeParallel switch is not being used with piping input
     if ($InvokeParallel.IsPresent -and $MyInvocation.ExpectingInput) {
 
-        Write-Warning "Cannot accept pipeline input while using the InvokeParallel switch."
+        Write-Warning 'Cannot accept pipeline input while using the InvokeParallel switch.'
+        break
+    }
+
+    if ($ComputerName.Count -eq 1 -and $InvokeParallel.IsPresent) {
+
+        Write-Warning 'The InvokeParallel switch cannot be used with only one computer name.'
         break
     }
 
