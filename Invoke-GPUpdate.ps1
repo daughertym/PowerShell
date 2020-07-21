7a0e5051ed630c8c90d06c9b3c05a6c96ecb596c
daughertym <66864919+daughertym@users.noreply.github.com>
2020-07-21 17:46:59 -0500

    Update Invoke-GPUpdate.ps1
    Invoke gpupdate /force on one or more computers.

--- Invoke-GPUpdate.ps1
+++ Invoke-GPUpdate.ps1
@@ -6,14 +6,11 @@ Invoke gpupdate /force on one or more computers.
 .PARAMETER ComputerName
 Specifies the computer(s) to invoke gpupdate /force on.
 
-.PARAMETER InvokeParallel
-Optional switch to Invoke-Command in parallel.
-
 .PARAMETER IncludeError
-Optional switch to include errors with InvokeParallel.
+Optional switch to include errors.
 
 .INPUTS
-String.
+None.
 
 .OUTPUTS
 None.
@@ -22,14 +19,11 @@ None.
 .\Invoke-GPUpdate -ComputerName PC01,PC02,PC03
 
 .EXAMPLE
-Get-Content C:\computers.txt | .\Invoke-GPUpdate
-
-.EXAMPLE
-.\Invoke-GPUpdate -ComputerName (Get-Content C:\computers.txt) -InvokeParallel
+.\Invoke-GPUpdate -ComputerName (Get-Content C:\computers.txt)
 
 .NOTES
 Author: Matthew D. Daugherty
-Date Modified: 17 July 2020
+Date Modified: 21 July 2020
 
 #>
 
@@ -37,35 +31,12 @@ Date Modified: 17 July 2020
 param (
 
     # Mandatory parameter for one or more computer names
-    [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [Parameter(Mandatory)]
     [string[]]
-    $ComputerName,
-
-    # Optional switch to Invoke-Command in parrallel
-    [Parameter()]
-    [switch]
-    $InvokeParallel,
-
-    # Optional switch to include errors with InvokeParallel
-    [Parameter()]
-    [switch]
-    $IncludeError
+    $ComputerName
 )
 
 begin {
-
-    # Make sure InvokeParallel switch is not being used with piping input
-    if ($InvokeParallel.IsPresent -and $MyInvocation.ExpectingInput) {
-
-        Write-Warning 'Cannot accept pipeline input while using the InvokeParallel switch.'
-        break
-    }
-
-    if ($ComputerName.Count -eq 1 -and $InvokeParallel.IsPresent) {
-
-        Write-Warning 'The InvokeParallel switch cannot be used with only one computer name.'
-        break
-    }
 
     # Scriptblock for Invoke-Command
     $InvokeCommandScriptBlock = {
@@ -79,45 +50,15 @@ begin {
 
 process {
 
-    switch ($InvokeParallel.IsPresent) {
+    # Parameters for Invoke-Command
+    $InvokeCommandParams = @{
 
-        'False' {
+        ComputerName = $ComputerName
+        ScriptBlock = $InvokeCommandScriptBlock
+        ErrorAction = 'SilentlyContinue'
+    }
 
-            foreach ($Computer in $ComputerName) {
-
-                $Computer = $Computer.ToUpper()
-
-                if (Test-Connection -ComputerName $Computer -Count 1 -Quiet) {
-
-                    try {
-                        
-                        Invoke-Command -ComputerName $Computer -ErrorAction Stop -ScriptBlock $InvokeCommandScriptBlock
-                    }
-                    catch {
-
-                        Write-Warning "Failed to Invoke-Command on $Computer"
-                    }
-
-                } # end if (Test-Connection)
-                else {
-
-                    Write-Warning "Test-Connection returned false on $Computer"
-                }
-
-            } # end foreach *$Computer in $ComputerName)
-        }
-        'True' {
-
-            # Parameters for Invoke-Command
-            $InvokeCommandParams = @{
-
-                ComputerName = $ComputerName
-                ScriptBlock = $InvokeCommandScriptBlock
-                ErrorAction = 'SilentlyContinue'
-            }
-
-            Invoke-Command @InvokeCommandParams
-        }
-
-    } # end switch ($InvokeParallel.IsPresent)
+    Invoke-Command @InvokeCommandParams
 }
+
+end {}
