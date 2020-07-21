07f8b666bbc962533cc6cbb24e3e6f8f8e87478a
daughertym <66864919+daughertym@users.noreply.github.com>
2020-07-20 20:27:46 -0500

    Update Set-Bios_Webcam_Mic.ps1
    Set BIOS webcam/microphone setting on HP computers.

--- Set-Bios_Webcam_Mic.ps1
+++ Set-Bios_Webcam_Mic.ps1
@@ -8,11 +8,20 @@ Set BIOS webcam/microphone setting on HP computers.
 .PARAMETER ComputerName
 Specifies the computer(s) to set BIOS setting on.
 
+.PARAMETER Webcam
+Specifies webcam setting (Enable/Disable)
+
+.PARAMETER Microphone
+Specifies microphone setting (Enable/Disable)
+
+.PARAMETER InvokeParallel
+Optional switch to Invoke-Command in parallel.
+
 .PARAMETER IncludeError
-Optional switch to include errors.
+Optional switch to include errors with InvokeParallel.
 
 .INPUTS
-You cannot pipe objects to Set-Bios_Webcam_Mic.ps1
+String
 
 .OUTPUTS
 System.Object
@@ -21,7 +30,7 @@ System.Object
 .\Set-Bios_Webcam_Mic.ps1 (Get-Content C:\computers.txt) -Webcam Enable -Microphone Enable
 
 .EXAMPLE
-.\Set-Bios_Webcam_Mic.ps1 (Get-Content C:\computers.txt) -Webcam Disable -Microphone Disable
+Get-Content C:\computers.txt | .\Set-Bios_Webcam_Mic.ps1 -Webcam Disable -Microphone Disable
 
 .EXAMPLE
 .\Set-Bios_Webcam_Mic.ps1 (Get-Content C:\computers.txt) -Webcam Enable -Microphone Enable -IncludeError -Verbose |
@@ -29,7 +38,7 @@ Export-Csv BiosSetting.csv -NoTypeInformation
 
 .NOTES
 Author: Matthew D. Daugherty
-Date Modified: 18 July 2020
+Date Modified: 20 July 2020
 
 #>
 
@@ -55,96 +64,174 @@ param (
     [string]
     $Microphone,
 
-    # Optional switch to include errors
-    [Parameter()]
-    [switch]
-    $IncludeError
+   # Optional switch to Invoke-Command in parrallel
+   [Parameter()]
+   [switch]
+   $InvokeParallel,
+
+   # Optional switch to include errors with InvokeParallel
+   [Parameter()]
+   [switch]
+   $IncludeError
 )
 
-# ScriptBlock for Invoke-Command
-$InvokeCommandScriptBlock = {
+begin {
 
-    $VerbosePreference = $Using:VerbosePreference
+    # Make sure InvokeParallel switch is not being used with piping input
+    if ($InvokeParallel.IsPresent -and $MyInvocation.ExpectingInput) {
 
-    Write-Verbose "Setting BIOS setting(s) on $env:COMPUTERNAME"
-
-    $Result = [PSCustomObject]@{
-
-        Webcam = $null
-        Microphone = $null
-        Error = $null
+        Write-Warning 'Cannot accept pipeline input while using the InvokeParallel switch.'
+        break
     }
 
-    # Change this variable to your BIOS password
-    $Password = 'password'
+    if ($ComputerName.Count -eq 1 -and $InvokeParallel.IsPresent) {
 
-    $Password_UTF = "<utf-16/>"+$Password
+        Write-Warning 'The InvokeParallel switch cannot be used with only one computer name.'
+        break
+    }
 
-    try {
+    # ScriptBlock for Invoke-Command
+    $InvokeCommandScriptBlock = {
 
-        # Parameters for Get-WmiObject
-        $GetWmiObjectParams = @{
+        $VerbosePreference = $Using:VerbosePreference
 
-            NameSpace = 'root/hp/instrumentedBIOS'
-            Class = 'HP_BiosSettingInterface'
-            ErrorAction = 'Stop'
+        Write-Verbose "Setting BIOS setting(s) on $env:COMPUTERNAME"
+
+        $Result = [PSCustomObject]@{
+
+            Webcam = $null
+            Microphone = $null
+            Error = $null
         }
 
-        $Bios = Get-WmiObject @GetWmiObjectParams
+        # Change this variable to your BIOS password
+        $Password = 'password'
 
-        # Set BIOS camera setting - Return code 0 = success
-        $Result.Webcam = ($Bios.SetBiosSetting('Integrated Camera', $Using:Webcam, $Password_UTF)).Return
+        $Password_UTF = "<utf-16/>"+$Password
 
-        # Set BIOS microphone setting - Return code 0 = success
-        $Result.Microphone = ($Bios.SetBiosSetting('Integrated Microphone', $Using:Microphone, $Password_UTF)).Return
-    }
-    catch {
+        try {
 
-        $Result.Error = $_.FullyQualifiedErrorId
-    }
+            # Parameters for Get-WmiObject
+            $GetWmiObjectParams = @{
 
-    $Result
-}
-
-# Parameters for Invoke-Command
-$InvokeCommandParams = @{
-
-    ComputerName = $ComputerName
-    ScriptBlock = $InvokeCommandScriptBlock
-    ErrorAction = 'SilentlyContinue'
-}
-
-if ($IncludeError.IsPresent) {
-
-    $InvokeCommandParams.Add('ErrorVariable','icmErrors')
-}
-
-Invoke-Command @InvokeCommandParams | ForEach-Object {
-
-    [PSCustomObject]@{
-
-        ComputerName = $_.PSComputerName.ToUpper()
-        InvokeStatus = 'Success'
-        Webcam = $_.Webcam
-        Microphone = $_.Microphone
-        Error = $_.Error
-    }
-}
-
-if ($IncludeError.IsPresent) {
-
-    if ($icmErrors) {
-
-        foreach ($icmError in $icmErrors) {
-
-            [PSCustomObject]@{
-
-                ComputerName = $icmError.TargetObject.ToUpper()
-                InvokeStatus = $icmError.FullyQualifiedErrorId
-                Webcam = $null
-                Microphone = $null
-                Error = $null
+                NameSpace = 'root/hp/instrumentedBIOS'
+                Class = 'HP_BiosSettingInterface'
+                ErrorAction = 'Stop'
             }
+
+            $Bios = Get-WmiObject @GetWmiObjectParams
+
+            # Set BIOS camera setting - Return code 0 = success
+            $Result.Webcam = ($Bios.SetBiosSetting('Integrated Camera', $Using:Webcam, $Password_UTF)).Return
+
+            # Set BIOS microphone setting - Return code 0 = success
+            $Result.Microphone = ($Bios.SetBiosSetting('Integrated Microphone', $Using:Microphone, $Password_UTF)).Return
         }
-    }
+        catch {
+
+            $Result.Error = $_.FullyQualifiedErrorId
+        }
+
+        $Result
+
+    } # end $InvokeCommandScriptBlock
 }
+
+process {
+
+    switch ($InvokeParallel.IsPresent) {
+
+        'False' {
+
+            foreach ($Computer in $ComputerName) {
+
+                $Result = [PSCustomObject]@{
+
+                    ComputerName = $Computer.ToUpper()
+                    TestConnection = $false
+                    InvokeStatus = $null
+                    Webcam = $null
+                    Microphone = $null
+                    Error = $null
+                }
+
+                if (Test-Connection -ComputerName $Computer -Count 1 -Quiet) {
+
+                    $Result.TestConnection = $true
+
+                    try {
+
+                        $InvokeResult = Invoke-Command -ComputerName $Computer -ErrorAction Stop -ScriptBlock $InvokeCommandScriptBlock
+
+                        $Result.InvokeStatus = 'Success'
+
+                        $Result.Webcam = $InvokeResult.Webcam
+
+                        $Result.Microphone = $InvokeResult.Microphone
+
+                        $Result.Error = $InvokeResult.Error
+                    }
+                    catch {
+
+                        $Result.InvokeStatus = $_.FullyQualifiedErrorId   
+                    }
+
+                } # end if (Test-Connection)
+
+                $Result
+
+            } # end foreach ($Computer in $ComputerName)
+        }
+        'True' {
+
+            # Parameters for Invoke-Command
+            $InvokeCommandParams = @{
+
+                ComputerName = $ComputerName
+                ScriptBlock = $InvokeCommandScriptBlock
+                ErrorAction = 'SilentlyContinue'
+            }
+
+            if ($IncludeError.IsPresent) {
+
+                $InvokeCommandParams.Add('ErrorVariable','icmErrors')
+            }
+
+            Invoke-Command @InvokeCommandParams | ForEach-Object {
+
+                [PSCustomObject]@{
+            
+                    ComputerName = $_.PSComputerName.ToUpper()
+                    InvokeStatus = 'Success'
+                    Webcam = $_.Webcam
+                    Microphone = $_.Microphone
+                    Error = $_.Error
+                }
+            }
+
+            if ($IncludeError.IsPresent) {
+
+                if ($icmErrors) {
+            
+                    foreach ($icmError in $icmErrors) {
+            
+                        [PSCustomObject]@{
+            
+                            ComputerName = $icmError.TargetObject.ToUpper()
+                            InvokeStatus = $icmError.FullyQualifiedErrorId
+                            Webcam = $null
+                            Microphone = $null
+                            Error = $null
+                        }
+                    }
+
+                } # # end if ($icmErrors)
+
+            } # end if ($IncludeError.IsPresent)
+        }
+
+    } # end switch ($InvokeParallel.IsPresent)
+
+} # end process
+
+end {}
