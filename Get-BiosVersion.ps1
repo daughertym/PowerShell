3501859fcbdc60d9f3d270f04889152a9d99ae34
daughertym <66864919+daughertym@users.noreply.github.com>
2020-07-20 20:55:00 -0500

    Create Get-BiosVersion.ps1
    Get BIOS version on one or more computers.

---
+++ Get-BiosVersion.ps1
@@ -0,0 +1,203 @@
+<#
+
+.SYNOPSIS
+Get BIOS version on one or more computers.
+
+.PARAMETER ComputerName
+Specifies the computer(s) to BIOS version on.
+
+.PARAMETER InvokeParallel
+Optional switch to Invoke-Command in parallel.
+
+.PARAMETER IncludeError
+Optional switch to include errors with InvokeParallel.
+
+.INPUTS
+String
+
+.OUTPUTS
+System.Object
+
+.EXAMPLE
+.\Get-BiosVersion (Get-Content C:\computers.txt)
+
+.EXAMPLE
+Get-Content C:\computers.txt | .\Get-BiosVersion
+
+.EXAMPLE
+.\Get-BiosVersion (Get-Content C:\computers.txt) -InvokeParallel
+
+.EXAMPLE
+.\Get-BiosVersion (Get-Content C:\computers.txt) -InvokeParallel -IncludeError
+
+.NOTES
+Author: Matthew D. Daugherty
+Date Modified: 20 July 2020
+
+#>
+
+[CmdletBinding()]
+param (
+
+    # Mandatory parameter for one or more computer names
+    [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string[]]
+    $ComputerName,
+
+    # Optional switch to Invoke-Command in parrallel
+    [Parameter()]
+    [switch]
+    $InvokeParallel,
+
+    # Optional switch to include errors with InvokeParallel
+    [Parameter()]
+    [switch]
+    $IncludeError
+)
+
+begin {
+
+    # Make sure InvokeParallel switch is not being used with piping input
+    if ($InvokeParallel.IsPresent -and $MyInvocation.ExpectingInput) {
+
+        Write-Warning 'Cannot accept pipeline input while using the InvokeParallel switch.'
+        break
+    }
+
+    if ($ComputerName.Count -eq 1 -and $InvokeParallel.IsPresent) {
+
+        Write-Warning 'The InvokeParallel switch cannot be used with only one computer name.'
+        break
+    }
+
+    # Scriptblock for Invoke-Command
+    $InvokeCommandScriptBlock = {
+
+        $VerbosePreference = $Using:VerbosePreference
+
+        Write-Verbose "Getting BIOS version on $env:COMPUTERNAME"
+
+        $Result = [PSCustomObject]@{
+
+            Manufacturer = $null
+            BiosVersion = $null
+            Error = $null
+        }
+
+        try {
+            
+            $BIOS = Get-CimInstance Win32_BIOS -Verbose:$false -ErrorAction Stop
+
+            $Result.Manufacturer = $BIOS.Manufacturer
+
+            $Result.BiosVersion = $BIOS.Name
+        }
+        catch {
+
+            $Result.Error = $_.FullyQualifiedErrorId
+        }
+
+        $Result
+
+    } # end $InvokeCommandScriptBlock
+}
+
+process {
+
+    switch ($InvokeParallel.IsPresent) {
+
+        'False' {
+
+            foreach ($Computer in $ComputerName) {
+
+                $Result = [PSCustomObject]@{
+
+                    ComputerName = $Computer.ToUpper()
+                    TestConnection = $false
+                    InvokeStatus = $null
+                    Manufacturer = $null
+                    BiosVersion = $null
+                    Error = $null
+                    
+                }
+
+                if (Test-Connection -ComputerName $Computer -Count 1 -Quiet) {
+
+                    $Result.TestConnection = $true
+
+                    try {
+                        
+                        $InvokeResult = Invoke-Command -ComputerName $Computer -ErrorAction Stop -ScriptBlock $InvokeCommandScriptBlock
+
+                        $Result.InvokeStatus = 'Success'
+
+                        $Result.Manufacturer = $InvokeResult.Manufacturer
+
+                        $Result.BiosVersion = $InvokeResult.BiosVersion
+
+                        $Result.Error = $InvokeResult.Error
+                    }
+                    catch {
+
+                        $Result.InvokeStatus = $_.FullyQualifiedErrorId
+                    }
+
+                } # end if (Test-Connection)
+
+                $Result
+
+            } # end foreach ($Computer in $ComputerName)
+        }
+        'True' {
+
+            # Parameters for Invoke-Command
+            $InvokeCommandParams = @{
+
+                ComputerName = $ComputerName
+                ScriptBlock = $InvokeCommandScriptBlock
+                ErrorAction = 'SilentlyContinue'
+            }
+
+            if ($IncludeError.IsPresent) {
+
+                $InvokeCommandParams.Add('ErrorVariable','icmErrors')
+            }
+    
+            Invoke-Command @InvokeCommandParams | ForEach-Object {
+    
+                [PSCustomObject]@{
+        
+                    ComputerName = $_.PSComputerName.ToUpper()
+                    InvokeStatus = 'Success'
+                    Manufacturer = $_.Manufacturer
+                    BiosVersion = $_.BiosVersion
+                    Error = $_.Error
+                }
+            }
+
+            if ($IncludeError.IsPresent) {
+
+                if ($icmErrors) {
+        
+                    foreach ($icmError in $icmErrors) {
+        
+                        [PSCustomObject]@{
+        
+                            ComputerName = $icmError.TargetObject.ToUpper()
+                            InvokeStatus = $icmError.FullyQualifiedErrorId
+                            Manufacturer = $null
+                            BiosVersion = $null
+                            Error = $null
+                        }
+                    }
+        
+                } # end if ($icmErrors)
+        
+            } # end if ($IncludeError.IsPresent)
+        }
+
+    } # end switch ($InvokeParallel.IsPresent)
+
+} # end process
+
+end {}
