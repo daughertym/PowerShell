6f78e508470fe3883be3473d75055a4559fb3ad7
daughertym <66864919+daughertym@users.noreply.github.com>
2020-07-25 18:36:31 -0500

    Update Invoke-Quser.ps1
    Invoke quser.exe on computers and get result as objects.

--- Invoke-Quser.ps1
+++ Invoke-Quser.ps1
@@ -1,21 +1,16 @@
 <#
 
 .SYNOPSIS
-Invoke quser.exe on one or more computers and return quser as an object.
-
-Use to see which users are logged on to computer(s).
+Invoke quser.exe on computers and get result as object.
 
 .PARAMETER ComputerName
-Specifies the computer(s) to invoke quser.exe on.
-
-.PARAMETER InvokeParallel
-Optional switch to Invoke-Command in parallel.
+Specifies the computers to query.
 
 .PARAMETER IncludeError
-Optional switch to include errors with InvokeParallel.
+Optional switch to include errors.
 
 .INPUTS
-String
+None. You cannot pipe objects.
 
 .OUTPUTS
 System.Object
@@ -24,217 +19,130 @@ System.Object
 .\Invoke-Quser -ComputerName PC01,PC02,PC03
 
 .EXAMPLE
-Get-Content C:\computers.txt | Invoke-Quser
+.\Invoke-Quser (Get-Content C:\computers.txt) -IncludeError
 
 .EXAMPLE
-.\Invoke-Quser (Get-Content C:\computers.txt) -InvokeParallel
-
-.EXAMPLE
-.\Invoke-Quser (Get-Content C:\computers.txt) -InvokeParallel -IncludeError
+.\Invoke-Quser (Get-Content C:\computers.txt) -Verbose | 
+Export-Csv Users.csv -NoTypeInformation
 
 .NOTES
 Author: Matthew D. Daugherty
-Date Modified: 21 July 2020
+Date Modified: 25 July 2020
 
 #>
 
 [CmdletBinding()]
 param (
 
-    # Mandatory parameter for one or more computer names
-    [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [Parameter(Mandatory)]
     [string[]]
     $ComputerName,
 
-    # Optional switch to Invoke-Command in parrallel
-    [Parameter()]
-    [switch]
-    $InvokeParallel,
-
-    # Optional switch to include errors with InvokeParallel
     [Parameter()]
     [switch]
     $IncludeError
 )
 
-begin {
+# Scriptblock for Invoke-Command
+$InvokeCommandScriptBlock = {
 
-    # Make sure InvokeParallel switch is not being used with piping input
-    if ($InvokeParallel.IsPresent -and $MyInvocation.ExpectingInput) {
+    $VerbosePreference = $Using:VerbosePreference
+    
+    Write-Verbose "Invoking quser.exe on $env:COMPUTERNAME"
 
-        Write-Warning 'Cannot accept pipeline input while using the InvokeParallel switch.'
-        break
-    }
+    function Convert-Quser {
 
-    if ($ComputerName.Count -eq 1 -and $InvokeParallel.IsPresent) {
+        # Modified this function from Reddit user: litemage
+    
+        # Function to convert quser result into an object
+    
+        $Quser = quser.exe 2>$null
 
-        Write-Warning 'The InvokeParallel switch cannot be used with only one computer name.'
-        break
-    }
+        if ($Quser) {
 
-    # Scriptblock for Invoke-Command
-    $InvokeCommandScriptBlock = {
-
-        $VerbosePreference = $Using:VerbosePreference
-        
-        Write-Verbose "Invoking quser.exe on $env:COMPUTERNAME"
+            foreach ($Line in $Quser) {
     
-        function Convert-Quser {
-    
-            # Modified this function from Reddit user: litemage
-        
-            # Function to convert quser result into an object
-        
-            $Quser = quser.exe 2>$null
-    
-            if ($Quser) {
-    
-                foreach ($Line in $Quser) {
-        
-                    if ($Line -match "LOGON TIME") {continue}
-                    
-                    [PSCustomObject]@{
-            
-                        UserName =  $Line.SubString(1, 20).Trim()
-                        State = $Line.SubString(46, 6).Trim()
-                        LogonTime = [datetime]$Line.SubString(65)
-                    }
-                }
-            }
-    
-        } # end Convert-Quser
-    
-        $QuserObject = Convert-Quser
-    
-        if ($QuserObject) {
-    
-            foreach ($User in $QuserObject) {
-    
+                if ($Line -match "LOGON TIME") {continue}
+                
                 [PSCustomObject]@{
-    
-                    UserName = $User.UserName
-                    State = $User.State
-                    LogonTime = $User.LogonTime
+        
+                    UserName =  $Line.SubString(1, 20).Trim()
+                    State = $Line.SubString(46, 6).Trim()
+                    LogonTime = [datetime]$Line.SubString(65)
                 }
             }
         }
-        else {
-    
+
+    } # end Convert-Quser
+
+    $QuserObject = Convert-Quser
+
+    if ($QuserObject) {
+
+        foreach ($User in $QuserObject) {
+
             [PSCustomObject]@{
-    
-                UserName = 'No user logged on'
-                State = $null
-                LogonTime = $null
+
+                UserLoggedOn = $true
+                UserName = $User.UserName
+                State = $User.State
+                LogonTime = $User.LogonTime
             }
         }
-    
-    } # end $InvokeCommandScriptBlock
+    }
+    else {
+
+        [PSCustomObject]@{
+
+            $UserLoggedOn = $false
+            UserName = $null
+            State = $null
+            LogonTime = $null
+        }
+    }
+} 
+
+# Parameters for Invoke-Command
+$InvokeCommandParams = @{
+
+    ComputerName = $ComputerName
+    ScriptBlock = $InvokeCommandScriptBlock
+    ErrorAction = 'SilentlyContinue'
 }
 
-process {
+if ($IncludeError.IsPresent) {
 
-    switch ($InvokeParallel.IsPresent) {
+    $InvokeCommandParams.Add('ErrorVariable','icmErrors')
+}
 
-        'False' {
+Invoke-Command @InvokeCommandParams | ForEach-Object {
 
-            foreach ($Computer in $ComputerName) {
+    [PSCustomObject]@{
 
-                $Result = [PSCustomObject]@{
-        
-                    ComputerName = $Computer.ToUpper()
-                    TestConnection = $false
-                    InvokeStatus = $null
-                    UserName = $null
-                    State = $null
-                    LogonTime = $null
-                }
+        ComputerName = $_.PSComputerName.ToUpper()
+        UserLoggedOn = $_.UserLoggedOn
+        UserName = $_.UserName
+        State = $_.State
+        LogonTime = $_.LogonTime
+        Error = $null
+    }
+}
 
-                if (Test-Connection -ComputerName $Computer -Count 1 -Quiet) {
+if ($IncludeError.IsPresent) {
 
-                    $Result.TestConnection = $true
+    if ($icmErrors) {
 
-                    try {
+        foreach ($icmError in $icmErrors) {
 
-                        $InvokeResult = Invoke-Command -ComputerName $Computer -ErrorAction Stop -ScriptBlock $InvokeCommandScriptBlock
+            [PSCustomObject]@{
 
-                        foreach ($User in $InvokeResult) {
-
-                            $Result.InvokeStatus = 'Success'
-
-                            $Result.UserName = $User.UserName
-
-                            $Result.State = $User.State
-
-                            $Result.LogonTime = $User.LogonTime
-
-                            $Result
-                        }
-                    }
-                    catch {
-
-                        $Result.InvokeStatus = $_.FullyQualifiedErrorId
-
-                        $Result
-                    }
-
-                } # end (Test-Connection)
-                else {
-
-                    $Result
-                }
-        
-            } # end foreach ($Computer in $ComputerName)
+                ComputerName = $icmError.TargetObject.ToUpper()
+                UserLoggedOn = $null
+                UserName = $null
+                State = $null
+                LogonTime = $null
+                Error = $icmError.FullyQualifiedErrorId
+            }
         }
-        'True' {
-
-            # Parameters for Invoke-Command
-            $InvokeCommandParams = @{
-
-                ComputerName = $ComputerName
-                ScriptBlock = $InvokeCommandScriptBlock
-                ErrorAction = 'SilentlyContinue'
-            }
-
-            if ($IncludeError.IsPresent) {
-
-                $InvokeCommandParams.Add('ErrorVariable','icmErrors')
-            }
-
-            Invoke-Command @InvokeCommandParams | ForEach-Object {
-
-                [PSCustomObject]@{
-            
-                    ComputerName = $_.PSComputerName.ToUpper()
-                    InvokeStatus = 'Success'
-                    UserName = $_.UserName
-                    State = $_.State
-                    LogonTime = $_.LogonTime
-                }
-            }
-
-            if ($IncludeError.IsPresent) {
-
-                if ($icmErrors) {
-            
-                    foreach ($icmError in $icmErrors) {
-            
-                        [PSCustomObject]@{
-            
-                            ComputerName = $icmError.TargetObject.ToUpper()
-                            InvokeStatus = $icmError.FullyQualifiedErrorId
-                            UserName = $null
-                            State = $null
-                            LogonTime = $null
-                        }
-                    }
-            
-                } # end if ($icmErrors)
-            
-            } # end if ($IncludeError.IsPresent)
-        }
-
-    } # end switch ($InvokeParallel.IsPresent)
-
-} # end process
-
-end {}
+    }
+}
