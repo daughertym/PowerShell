7b79eefb7dc8a0d3a57d9affaa179b590190b3d5
daughertym <66864919+daughertym@users.noreply.github.com>
2020-07-25 18:45:08 -0500

    Update New-URLShortcut.ps1
    Create url shortcut on computers.

--- New-URLShortcut.ps1
+++ New-URLShortcut.ps1
@@ -1,7 +1,7 @@
 <#
 
 .SYNOPSIS
-Create url shortcut on one or more computers at a specified destination.
+Create url shortcut on computers.
 
 For example:
 
@@ -19,14 +19,11 @@ Specifies the URL for the url shortcut.
 .PARAMETER Destination
 Specifies the destination for the url shortcut.
 
-.PARAMETER InvokeParallel
-Optional switch to Invoke-Command in parallel.
-
 .PARAMETER IncludeError
-Optional switch to include errors with InvokeParallel.
+Optional switch to include errors.
 
 .INPUTS
-String
+None. You cannot pipe objects.
 
 .OUTPUTS
 System.Object
@@ -35,20 +32,14 @@ System.Object
 New-URLShortcut -ComputerName PC01,PC02,PC03 -Name Reddit -URL www.reddit.com -Destination C:\Users\Public\Desktop
 
 .EXAMPLE
-New-URLShortcut -ComputerName (Get-Content C:\computers.txt) -Name Reddit -URL www.reddit.com -Destination C:\Users\Public\Desktop
+New-URLShortcut (Get-Content C:\computers.txt) -Name Reddit -URL www.reddit.com -Destination C:\Users\Public\Desktop
 
 .EXAMPLE
-Get-Content C:\computers.txt | New-URLShortcut -Name Reddit -URL www.reddit.com -Destination C:\Users\Public\Desktop
-
-.EXAMPLE
-New-URLShortcut -ComputerName PC01,PC02,PC03 -Name Reddit -URL www.reddit.com -Destination C:\Users\Public\Desktop -InvokeParallel
-
-.EXAMPLE
-New-URLShortcut -ComputerName PC01,PC02,PC03 -Name Reddit -URL www.reddit.com -Destination C:\Users\Public\Desktop -InvokeParallel -IncludeError
+New-URLShortcut -ComputerName PC01,PC02,PC03 -Name Reddit -URL www.reddit.com -Destination C:\Users\Public\Desktop -IncludeError
 
 .NOTES
 Author: Matthew D. Daugherty
-Date Modified: 20 July 2020
+Date Modified: 25 July 2020
 
 #>
 
@@ -56,180 +47,99 @@ Date Modified: 20 July 2020
 [CmdletBinding()]
 param (
 
-    # Mandatory parameter for one or more computer names
     [Parameter(Mandatory)]
     [string[]]
     $ComputerName,
 
-    # Mandatory parameter for one or more computer names
     [Parameter(Mandatory)]
     [string]
     $Name,
 
-    # Mandatory parameter for URL for shortcut
     [Parameter(Mandatory)]
     [string]
     $URL,
 
-    # Mandatory parameter for destination for shortcut
     [Parameter(Mandatory)]
     [string]
     $Destination,
 
-        # Optional switch to Invoke-Command in parrallel
-    [Parameter()]
-    [switch]
-    $InvokeParallel,
-
-    # Optional switch to include errors with InvokeParallel
     [Parameter()]
     [switch]
     $IncludeError
-
 )
 
-begin {
 
-    # Make sure InvokeParallel switch is not being used with piping input
-    if ($InvokeParallel.IsPresent -and $MyInvocation.ExpectingInput) {
+# Scriptblock for Invoke-Command
+$InvokeCommandScriptBlock = {
 
-        Write-Warning 'Cannot accept pipeline input while using the InvokeParallel switch.'
-        break
+    $VerbosePreference = $Using:VerbosePreference
+    
+    Write-Verbose "Creating $Using:URL shortcut on $env:COMPUTERNAME"
+
+    $URLShortcutFile = "$($Using:Destination)\$($Using:Name).url"
+
+    $WScriptShell = New-Object -ComObject WScript.Shell
+
+    $URLShortcut = $WScriptShell.CreateShortcut($URLShortcutFile)
+
+    $URLShortcut.TargetPath = $Using:URL
+
+    $URLShortcut.Save()
+
+    $Result = [PSCustomObject]@{
+
+        ShortcutCreated = $false
     }
 
-    if ($ComputerName.Count -eq 1 -and $InvokeParallel.IsPresent) {
+    if (Test-Path -Path $URLShortcutFile) {
 
-        Write-Warning 'The InvokeParallel switch cannot be used with only one computer name.'
-        break
+        $Result.ShortcutCreated = $true
     }
 
-    # Scriptblock for Invoke-Command
-    $InvokeCommandScriptBlock = {
-
-        $VerbosePreference = $Using:VerbosePreference
-        
-        Write-Verbose "Creating URL shortcut on $env:COMPUTERNAME"
-
-        $URLShortcutFile = "$($Using:Destination)\$($Using:Name).url"
-
-        $WScriptShell = New-Object -ComObject WScript.Shell
-
-        $URLShortcut = $WScriptShell.CreateShortcut($URLShortcutFile)
-
-        $URLShortcut.TargetPath = $Using:URL
-
-        $URLShortcut.Save()
-
-        $Result = [PSCustomObject]@{
-
-            ShortcutCreated = $false
-        }
-
-        if (Test-Path -Path $URLShortcutFile) {
-
-            $Result.ShortcutCreated = $true
-        }
-
-        $Result
-
-    } # end $InvokeCommandScriptBlock
+    $Result
 }
 
-process {
 
-    switch ($InvokeParallel.IsPresent) {
 
-        'False' {
+# Parameters for Invoke-Command
+$InvokeCommandParams = @{
 
-            foreach ($Computer in $ComputerName) {
+    ComputerName = $ComputerName
+    ScriptBlock = $InvokeCommandScriptBlock
+    ErrorAction = 'SilentlyContinue'
+}
 
-                $Result = [PSCustomObject]@{
+if ($IncludeError.IsPresent) {
 
-                    ComputerName = $Computer.ToUpper()
-                    TestConnection = $false
-                    InvokeStatus = $null
-                    Name = $null
-                    URL = $null
-                    ShortcutCreated = $null
-                }
+    $InvokeCommandParams.Add('ErrorVariable','icmErrors')
+}
 
-                if (Test-Connection -ComputerName $Computer -Count 1 -Quiet) {
+Invoke-Command @InvokeCommandParams | ForEach-Object {
 
-                    $Result.TestConnection = $true
+    [PSCustomObject]@{
 
-                    try {
-                        
-                        $InvokeResult = Invoke-Command -ComputerName $Computer -ErrorAction Stop -ScriptBlock $InvokeCommandScriptBlock
+        ComputerName = $_.PSComputerName.ToUpper()
+        Name = $Name
+        URL = $URL
+        ShortcutCreated = $_.ShortcutCreated
+        Error = $null
+    }
+}
 
-                        $Result.InvokeStatus = 'Success'
+if ($IncludeError.IsPresent) {
 
-                        $Result.Name = $Name
+    if ($icmErrors) {
 
-                        $Result.URL = $URL
+        foreach ($icmError in $icmErrors) {
 
-                        $Result.ShortcutCreated = $InvokeResult.ShortcutCreated
-                    }
-                    catch {
+            [PSCustomObject]@{
 
-                        $Result.InvokeStatus = $_.FullyQualifiedErrorId
-                    }
-
-                } # end if (Test-Connection)
-
-                $Result
-
-            } # end foreach ($Computer in $ComputerName)
+                ComputerName = $icmError.TargetObject.ToUpper()
+                Name = $null
+                URL = $null
+                ShortcutCreated = $null
+                Error = $icmError.FullyQualifiedErrorId
+            }
         }
-        'True' {
-
-            # Parameters for Invoke-Command
-            $InvokeCommandParams = @{
-
-                ComputerName = $ComputerName
-                ScriptBlock = $InvokeCommandScriptBlock
-                ErrorAction = 'SilentlyContinue'
-            }
-
-            if ($IncludeError.IsPresent) {
-
-                $InvokeCommandParams.Add('ErrorVariable','icmErrors')
-            }
-
-            Invoke-Command @InvokeCommandParams | ForEach-Object {
-
-                [PSCustomObject]@{
-        
-                    ComputerName = $_.PSComputerName.ToUpper()
-                    InvokeStatus = 'Success'
-                    Name = $Name
-                    URL = $URL
-                    ShortcutCreated = $_.ShortcutCreated
-                }
-            }
-
-            if ($IncludeError.IsPresent) {
-
-                if ($icmErrors) {
-        
-                    foreach ($icmError in $icmErrors) {
-            
-                        [PSCustomObject]@{
-            
-                            ComputerName = $icmError.TargetObject.ToUpper()
-                            InvokeStatus = $icmError.FullyQualifiedErrorId
-                            Name = $null
-                            URL = $null
-                            ShortcutCreated = $null
-                        }
-                    }
-        
-                } # end if ($icmErrors)
-        
-            } # end if ($IncludeError.IsPresent)
-        }
-
-    } # end switch ($InvokeParallel.IsPresent)
-
-} # end process
-
-end {}
+    }
+}
