10e069172f6cc53dcc358c8c8ea15524533f0908
daughertym <66864919+daughertym@users.noreply.github.com>
2020-07-27 03:17:58 -0500

    Update Send-Message.ps1
    Send message to computers.

--- Send-Message.ps1
+++ Send-Message.ps1
@@ -1,10 +1,10 @@
 <#
 
 .SYNOPSIS
-Send message to one or more computers.
+Send message to computers.
 
 .PARAMETER ComputerName
-Specifies the computer(s) to send message to.
+Specifies the computers to send message to.
 
 .PARAMETER Message
 Specifies the message to send.
@@ -15,171 +15,77 @@ If message contains spaces it must be surrounded by single or double quotes.
 
 "This is an example message"
 
-.PARAMETER InvokeParallel
-Optional switch to Invoke-Command in parallel.
-
-.PARAMETER IncludeError
-Optional switch to include errors with InvokeParallel.
-
 .PARAMETER AsLoop
 Optional switch to send message as a continuous loop.
 
-Messages will send every 15 minutes.
+Message will send every 15 minutes.
 
 .INPUTS
-String
+None. You cannot pipe objects.
 
 .OUTPUTS
 None.
 
 .EXAMPLE
-.\Send-Message -ComputerName PC01,PC01,PC03 -Message 'Hello World!'
+.\Send-Message -ComputerName PC01 -Message 'Hello World!'
 
 .EXAMPLE
-Get-Content .\computers.txt | .\Send-Message -Message 'Hello World!' -AsLoop
-
-.EXAMPLE
-.\Send-Message (Get-Content .\computers.txt) -Message 'Hello World!' -InvokeParallel
-
-.EXAMPLE
-.\Send-Message (Get-Content .\computers.txt) -Message 'Hello World!' -InvokeParallel -AsLoop
+.\Send-Message -ComputerName (Get-Content C:\computers.txt) -Message 'Hello World!' -ErrorAction SilentlyContinue
 
 .NOTES
 Author: Matthew D. Daugherty
-Date Modified: 17 July 2020
+Date Modified: 27 July 2020
 
 #>
 
 [CmdletBinding()]
 param (
 
-    # Mandatory parameter for one or more computer names
-    [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [Parameter(Mandatory)]
     [string[]]
     $ComputerName,
 
-    # Mandatory parameter for message
     [Parameter(Mandatory)]
     [string]
     $Message,
 
-    # Optional switch to send message as a continuous loop
     [Parameter()]
     [switch]
-    $AsLoop,
-
-    # Optional switch to Invoke-Command in parrallel
-    [Parameter()]
-    [switch]
-    $InvokeParallel
+    $AsLoop
 )
 
-begin {
+# Scriptblock for Invoke-Command
+$InvokeCommandScriptBlock = {
 
-    # Make sure InvokeParallel switch is not being used with piping input
-    if ($InvokeParallel.IsPresent -and $MyInvocation.ExpectingInput) {
+    msg.exe * $Using:Message
 
-        Write-Warning 'Cannot accept pipeline input while using the InvokeParallel switch.'
-        break
-    }
-
-    if ($ComputerName.Count -eq 1 -and $InvokeParallel.IsPresent) {
-
-        Write-Warning 'The InvokeParallel switch cannot be used with only one computer name.'
-        break
-    }
-
-    # Scriptblock for Invoke-Command
-    $InvokeCommandScriptBlock = {
-
-        msg.exe * $Using:Message
-
-        Write-Verbose "Message sent to $env:COMPUTERNAME" -Verbose
-
-    } # end $InvokeCommandScriptBlock
+    Write-Verbose "Message sent to $env:COMPUTERNAME." -Verbose
 }
 
-process {
+# Parameters for Invoke-Command
+$InvokeCommandParams = @{
 
-    switch ($InvokeParallel.IsPresent) {
+    ComputerName = $ComputerName
+    ScriptBlock = $InvokeCommandScriptBlock
+    ErrorAction = $ErrorActionPreference
+}
 
-        'False' {
+switch ($AsLoop.IsPresent) {
 
-            do {
+    'True' {
 
-                foreach ($Computer in $ComputerName) {
+        while ($true) {
 
-                    try {
+            Clear-Host
 
-                        Invoke-Command -ComputerName $Computer -ErrorAction Stop -ScriptBlock $InvokeCommandScriptBlock
-                    }
-                    catch {
+            Invoke-Command @InvokeCommandParams
 
-                        Write-Host "Failed to send message to $($Computer.ToUpper())" -ForegroundColor Red
-                    }
-                    
-                } # end foreach ($Computer in $ComputerName)
-    
-                if ($AsLoop.IsPresent) {
-    
-                    # Sleep 15 minutes
-                    Start-Sleep -Seconds 3
-    
-                    Clear-Host
-    
-                    $Loop = $true
-                }
-                else {
-    
-                    $Loop = $false
-                }
-    
-            } until ($Loop -eq $false)
+            # Sleep for 15 minutes
+            Start-Sleep -Seconds 900
         }
-        'True' {
+    }
+    'False' {
 
-            # Parameters for Invoke-Command
-            $InvokeCommandParams = @{
-
-                ComputerName = $ComputerName
-                ScriptBlock = $InvokeCommandScriptBlock
-                ErrorAction = 'SilentlyContinue'
-                ErrorVariable = 'icmErrors'
-            }
-
-            do {
-
-                Invoke-Command @InvokeCommandParams
-
-                if ($icmErrors) {
-
-                    foreach ($icmError in $icmErrors) {
-    
-                        Write-Host "Failed to send message to $($icmError.TargetObject.ToUpper())" -ForegroundColor Red
-                    }
-                }
-
-                if ($AsLoop.IsPresent) {
-    
-                    # Sleep 15 minutes
-                    Start-Sleep -Seconds 3
-    
-                    Clear-Host
-
-                    Clear-Variable -Name $icmErrors
-    
-                    $Loop = $true
-                }
-                else {
-    
-                    $Loop = $false
-                }
-
-            } until ($Loop -eq $false)
-        }
-
-    } # end switch ($InvokeParallel.IsPresent)
-
-} # end process
-
-end {}
+        Invoke-Command @InvokeCommandParams
+    }
+}
