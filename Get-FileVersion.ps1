ccd89078c4a5dc9dbc0fa4225e22734b81a0345f
daughertym <66864919+daughertym@users.noreply.github.com>
2020-07-27 02:56:35 -0500

    Create Get-FileVersion.ps1
    Get file version info from computers.

---
+++ Get-FileVersion.ps1
@@ -0,0 +1,122 @@
+ <#
+.SYNOPSIS
+Get file version info from computers.
+
+.PARAMETER ComputerName
+Specifies the computers to query.
+
+.PARAMETER FilePath
+Specifies the file to query.
+
+.PARAMETER IncludeNonResponding
+Optional switch to include nonresponding computers.
+
+.INPUTS
+None. You cannot pipe objects.
+
+.OUTPUTS
+System.Object
+
+.EXAMPLE
+.\Get-FileVersion -ComputerName PC01,PC02,PC03 -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+
+.EXAMPLE
+.\Get-FileVersion (Get-Content .\computers.txt) -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -ErrorAction SilentlyContinue
+
+.EXAMPLE
+.\Get-FileVersion (Get-Content .\computers.txt) -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -IncludeNonResponding -Verbose |
+Export-Csv Chrome.csv -NoTypeInformation
+
+.NOTES
+Author: Matthew D. Daugherty
+Date Modified: 27 July 2020
+
+#>
+
+[CmdletBinding()]
+param (
+
+    [Parameter(Mandatory)]
+    [string[]]
+    $ComputerName,
+
+    [Parameter(Mandatory)]
+    [string]
+    $FilePath,
+
+    [Parameter()]
+    [switch]
+    $IncludeNonResponding
+)
+
+$FileName = Split-Path -Path $FilePath -Leaf
+
+# Script block for Invoke-Command
+$InvokeCommandScriptBlock = {
+
+    $VerbosePreference = $Using:VerbosePreference
+    
+    Write-Verbose "Querying $Using:FileName on $env:COMPUTERNAME."
+
+    $Result = [PSCustomObject]@{
+
+        ComputerName = $env:COMPUTERNAME
+        FileName = $Using:FileName
+        FileExists = $false
+        FileVersion = $null
+        LastWriteTime = $null
+    }
+
+    if (Test-Path -Path $Using:FilePath) {
+
+        $Result.FileExists = $true
+
+        $File = Get-Item -Path $Using:FilePath
+
+        $Result.FileVersion = $File.VersionInfo.FileVersionRaw
+
+        $Result.LastWriteTime = $File.LastWriteTime
+    }
+
+    $Result
+}
+
+# Parameters for Invoke-Command
+$InvokeCommandParams = @{
+
+    ComputerName = $ComputerName
+    ScriptBlock = $InvokeCommandScriptBlock
+    ErrorAction = $ErrorActionPreference
+}
+
+switch ($IncludeNonResponding.IsPresent) {
+
+    'True' {
+
+        $InvokeCommandParams.Add('ErrorVariable','NonResponding')
+
+        Invoke-Command @InvokeCommandParams | 
+        Select-Object -Property *, ErrorId -ExcludeProperty PSComputerName, PSShowComputerName, RunspaceId
+
+        if ($NonResponding) {
+
+            foreach ($Computer in $NonResponding) {
+
+                [PSCustomObject]@{
+
+                    ComputerName = $Computer.TargetObject.ToUpper()
+                    FileName = $null
+                    FileExists = $null
+                    FileVersion = $null
+                    LastWriteTime = $null
+                    ErrorId = $Computer.FullyQualifiedErrorId
+                }
+            }
+        }
+    }
+    'False' {
+
+        Invoke-Command @InvokeCommandParams | 
+        Select-Object -Property * -ExcludeProperty PSComputerName, PSShowComputerName, RunspaceId
+    }
+}
