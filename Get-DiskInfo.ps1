7ef1f7d9049f4bfe0ffabb24a5b3d737f39471b0
daughertym <66864919+daughertym@users.noreply.github.com>
2020-07-27 02:55:07 -0500

    Update Get-DiskInfo.ps1
    Get disk info from computers.

--- Get-DiskInfo.ps1
+++ Get-DiskInfo.ps1
@@ -19,7 +19,7 @@ System.Object
 .\Get-DiskInfo -ComputerName PC01,PC02,PC03
 
 .EXAMPLE
-.\Get-DiskInfo (Get-Content C:\computers.txt)
+.\Get-DiskInfo (Get-Content C:\computers.txt) -ErrorAction SilentlyContinue
 
 .EXAMPLE
 .\Get-DiskInfo (Get-Content C:\computers.txt) -IncludeNonResponding -Verbose |
@@ -27,7 +27,7 @@ Export-Csv DiskInfo.csv -NoTypeInformation
 
 .NOTES
 Author: Matthew D. Daugherty
-Date Modified: 26 July 2020
+Date Modified: 27 July 2020
 
 #>
 
@@ -77,7 +77,7 @@ $InvokeCommandParams = @{
 
     ComputerName = $ComputerName
     ScriptBlock = $InvokeCommandScriptBlock
-    ErrorAction = 'SilentlyContinue'
+    ErrorAction = $ErrorActionPreference
 }
 
 switch ($IncludeNonResponding.IsPresent) {
