a5119db10c4ddaecdd2fd58e840dbc9c917e7996
daughertym <66864919+daughertym@users.noreply.github.com>
2020-07-27 02:39:31 -0500

    Update Get-LocalUserInfo.ps1
    Get local user account info from computers.

--- Get-LocalUserInfo.ps1
+++ Get-LocalUserInfo.ps1
@@ -1,227 +1,126 @@
 <#
 
 .SYNOPSIS
-Get local user account info from one or more computers.
+Get local user account info from computers.
 
 .PARAMETER ComputerName
-Specifies the computer(s) to get local user account info from.
+Specifies the computers to query.
 
-.PARAMETER InvokeParallel
-Optional switch to Invoke-Command in parallel.
-
-.PARAMETER IncludeError
-Optional switch to include errors with InvokeParallel.
+.PARAMETER IncludeNonResponding
+Optional switch to include nonresponding computers.
 
 .INPUTS
-String
+None. You cannot pipe objects.
 
 .OUTPUTS
 System.Object
 
 .EXAMPLE
-.\Get-LocalUserInfo (Get-Content .\computers.txt)
+.\Get-LocalUserInfo | Where-Object Enabled
 
 .EXAMPLE
-Get-Content .\computers.txt | .\Get-LocalUserInfo -FilePath
+.\Get-LocalUserInfo -ComputerName PC01,PC02,PC03
 
 .EXAMPLE
-.\Get-FileVersion (Get-Content .\computers.txt) -InvokeParallel
-
-.EXAMPLE
-.\Get-FileVersion (Get-Content .\computers.txt) -InvokeParallel -IncludeError
+.\Get-LocalUserInfo (Get-Content .\computers.txt) -IncludeNonResponding -Verbose |
+Export-Csv UserInfo.csv -NoTypeInformation
 
 .NOTES
 Author: Matthew D. Daugherty
-Date Modified: 17 July 2020
+Date Modified: 27 July 2020
 
 #>
 
 [CmdletBinding()]
 param (
 
-    # Parameter for one or more computer names
-    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [Parameter()]
     [string[]]
     $ComputerName = $env:COMPUTERNAME,
 
-     # Optional switch to Invoke-Command in parrallel
-     [Parameter()]
-     [switch]
-     $InvokeParallel,
- 
-     # Optional switch to include errors with InvokeParallel
-     [Parameter()]
-     [switch]
-     $IncludeError
+    [Parameter()]
+    [switch]
+    $IncludeNonResponding
 )
 
-begin {
+# Scriptblock for Invoke-Command
+$InvokeCommandScriptBlock = {
 
-    # Make sure InvokeParallel switch is not being used with piping input
-    if ($InvokeParallel.IsPresent -and $MyInvocation.ExpectingInput) {
+    $VerbosePreference = $Using:VerbosePreference
+    
+    Write-Verbose "Getting local user info on $env:COMPUTERNAME"
 
-        Write-Warning 'Cannot accept pipeline input while using the InvokeParallel switch.'
-        break
-    }
+    $ResultsArray = @()
 
-    if ($ComputerName.Count -eq 1 -and $InvokeParallel.IsPresent) {
+    foreach ($LocalUser in Get-LocalUser) {
 
-        Write-Warning 'The InvokeParallel switch cannot be used with only one computer name.'
-        break
-    }
+        $LocalUserInfo = [PSCustomObject]@{
 
-    # Scriptblock for Invoke-Command
-    $InvokeCommandScriptBlock = {
-
-        $VerbosePreference = $Using:VerbosePreference
-        
-        Write-Verbose "Getting local user info on $env:COMPUTERNAME"
-    
-        $ResultsArray = @()
-    
-        foreach ($LocalUser in Get-LocalUser) {
-    
-            $LocalUserInfo = [PSCustomObject]@{
-    
-                LocalUser = $LocalUser.Name
-                LastLogon = $LocalUser.LastLogon
-                Enabled = $LocalUser.Enabled
-                GroupMembership = $null
-            }
-    
-            $GroupArray = @()
-    
-            foreach ($Group in Get-LocalGroup) {
-        
-                # If local user is a member of the current group in loop
-                if (Get-LocalGroupMember -Name $Group -Member $LocalUser -ErrorAction SilentlyContinue) {
-    
-                    # Add the name of the group to $GroupArray
-                    $GroupArray += $Group.Name 
-                }
-            }
-    
-            $GroupMembership = $GroupArray -join ", "
-    
-            $LocalUserInfo.GroupMembership = $GroupMembership
-    
-            $ResultsArray += $LocalUserInfo
+            ComputerName = $env:COMPUTERNAME
+            LocalUser = $LocalUser.Name
+            LastLogon = $LocalUser.LastLogon
+            Enabled = $LocalUser.Enabled
+            GroupMembership = $null
         }
+
+        $GroupArray = @()
+
+        foreach ($Group in Get-LocalGroup) {
     
-        $ResultsArray
-    
-    } # end $InvokeCommandScriptBlock
+            # If local user is a member of the current group in loop
+            if (Get-LocalGroupMember -Name $Group -Member $LocalUser -ErrorAction SilentlyContinue) {
+
+                # Add the name of the group to $GroupArray
+                $GroupArray += $Group.Name 
+            }
+        }
+
+        $GroupMembership = $GroupArray -join ", "
+
+        $LocalUserInfo.GroupMembership = $GroupMembership
+
+        $ResultsArray += $LocalUserInfo
+    }
+
+    $ResultsArray
 }
 
-process {
+# Parameters for Invoke-Command
+$InvokeCommandParams = @{
 
-    switch ($InvokeParallel.IsPresent) {
+    ComputerName = $ComputerName
+    ScriptBlock = $InvokeCommandScriptBlock
+    ErrorAction = 'SilentlyContinue'
+}
 
-        'False' {
+switch ($IncludeNonResponding.IsPresent) {
 
-            foreach ($Computer in $ComputerName) {
+    'True' {
 
-                $Result = [PSCustomObject]@{
-        
-                    ComputerName = $Computer.ToUpper()
-                    TestConnection = $false
-                    InvokeStatus = $null
+        $InvokeCommandParams.Add('ErrorVariable','NonResponding')
+
+        Invoke-Command @InvokeCommandParams | 
+        Select-Object -Property *, ErrorId -ExcludeProperty PSComputerName, PSShowComputerName, RunspaceId
+
+        if ($NonResponding) {
+
+            foreach ($Computer in $NonResponding) {
+
+                [PSCustomObject]@{
+
+                    ComputerName = $Computer.TargetObject.ToUpper()
                     LocalUser = $null
                     LastLogon = $null
                     Enabled = $null
                     GroupMembership = $null
+                    ErrorId = $Computer.FullyQualifiedErrorId
                 }
-        
-                if (Test-Connection -ComputerName $Computer -Count 1 -Quiet) {
-        
-                    $Result.TestConnection = $true
-        
-                    try {
-        
-                        $InvokeResult = Invoke-Command -ComputerName $Computer -ErrorAction Stop -ScriptBlock $InvokeCommandScriptBlock
-        
-                        $InvokeResult | ForEach-Object {
-        
-                            $Result.InvokeStatus = 'Success'
-        
-                            $Result.LocalUser = $_.LocalUser
-        
-                            $Result.LastLogon = $_.LastLogon
-        
-                            $Result.Enabled = $_.Enabled
-        
-                            $Result.GroupMembership = $_.GroupMembership
-        
-                            $Result
-                        }
-                    }
-                    catch {
-        
-                        $Result.InvokeStatus = $_.FullyQualifiedErrorId
-        
-                        $Result
-                    }
-        
-                } # end if (Test-Connection)
-                else {
-        
-                    $Result
-                }
-        
-            } # end foreach ($Computer in $ComputerName)
+            }
         }
-        'True' {
+    }
+    'False' {
 
-            # Parameters for Invoke-Command
-            $InvokeCommandParams = @{
-
-                ComputerName = $ComputerName
-                ScriptBlock = $InvokeCommandScriptBlock
-                ErrorAction = 'SilentlyContinue'
-            }
-
-            if ($IncludeError.IsPresent) {
-
-                $InvokeCommandParams.Add('ErrorVariable','icmErrors')
-            }
-
-            Invoke-Command @InvokeCommandParams | ForEach-Object {
-
-                [PSCustomObject]@{
-            
-                    ComputerName = $_.PSComputerName.ToUpper()
-                    InvokeStatus = 'Success'
-                    LocalUser = $_.LocalUser
-                    LastLogon = $_.LastLogon
-                    Enabled = $_.Enabled
-                    GroupMembership = $_.GroupMembership
-                }
-            }
-
-            if ($IncludeError.IsPresent) {
-
-                if ($icmErrors) {
-            
-                    foreach ($icmError in $icmErrors) {
-            
-                        [PSCustomObject]@{
-            
-                            ComputerName = $icmError.TargetObject.ToUpper()
-                            InvokeStatus = $icmError.FullyQualifiedErrorId
-                            LocalUser = $null
-                            LastLogon = $null
-                            Enabled = $null
-                            GroupMembership = $null
-                        }
-                    }
-            
-                } # end if ($icmErrors)
-            
-            } # end if ($IncludeError.IsPresent)
-        }
-
-    } # end switch ($InvokeParallel.IsPresent)
-
-} # end process
-
-end {}
+        Invoke-Command @InvokeCommandParams | 
+        Select-Object -Property * -ExcludeProperty PSComputerName, PSShowComputerName, RunspaceId
+    }
+}
