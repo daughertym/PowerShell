6ab55729fd87f80728c47eeaa1f15c9629af8e6d
daughertym <66864919+daughertym@users.noreply.github.com>
2020-08-02 22:35:29 -0500

    Update Get-BiosVersion.ps1
    Get BIOS version from computers.

--- Get-BiosVersion.ps1
+++ Get-BiosVersion.ps1
@@ -16,6 +16,9 @@ None. You cannot pipe objects.
 System.Object
 
 .EXAMPLE
+.\Get-BiosVersion
+
+.EXAMPLE
 .\Get-BiosVersion -ComputerName PC01,PC02,PC03
 
 .EXAMPLE
@@ -27,16 +30,16 @@ Export-Csv BiosVersion.csv -NoTypeInformation
 
 .NOTES
 Author: Matthew D. Daugherty
-Date Modified: 27 July 2020
+Date Modified: 2 August 2020
 
 #>
 
 [CmdletBinding()]
 param (
 
-    [Parameter(Mandatory)]
+    [Parameter()]
     [string[]]
-    $ComputerName,
+    $ComputerName = $env:COMPUTERNAME,
 
     [Parameter()]
     [switch]
